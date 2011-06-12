4e5eb2df9a887dd7ed6f85f42b9c7bb235ebcdf8
Karen Rustad <karen.rustad@gmail.com>
2011-06-11 23:48:31 -0700

    Made list template dynamic

--- taskitte/kittehs/urls.py
+++ taskitte/kittehs/urls.py
@@ -1,14 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
     (r'^$', "kittehs.views.index"),
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    (r'^(?P<list_pk>\d+)/$', "kittehs.views.list"),
 )
